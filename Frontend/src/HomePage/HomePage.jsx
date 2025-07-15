8257ceff131e866ba89829b73b7cafecfcc07742
Parth Ramani <parthramani63@gmail.com>
2025-07-16 01:28:55 +0530

    solve error

--- Frontend/src/HomePage/HomePage.jsx
+++ Frontend/src/HomePage/HomePage.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import NavBar from './NavBar';
 import Body from './Body';
-import Footer from './Footer';
 
 function HomePage() {
     return ( 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,11 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import HomePage from './HomePage/HomePage.jsx'
-// import { createRoot } from 'react-dom/client'
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import NavBar from './HomePage/NavBar.jsx'
-import Footer from './HomePage/Footer.jsx'
-import ShowPath from './HomePage/ShowPath.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -15,6 +12,5 @@ createRoot(document.getElementById('root')).render(
       <Route path='/' element={<HomePage/>}></Route>
       <Route path='/Allcon' element={<HomePage/>}></Route>
     </Routes>
-    {/* <Footer/> */}
   </BrowserRouter>
 )
